1a38bd6878849ae43b6542c32e3fc1e829c1452e
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2020-10-01 17:05:15 -0400

    class-based-generic-api-views created

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,29 +1,13 @@
 from apps.posts.models import Topic, Post
 from apps.posts.serializers import PostSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class PostDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
